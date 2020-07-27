25d4f127dd5d80079f0b5e489750f2305c3f12c5
Mike Sapienza <mikesapaudio@gmail.com>
2020-07-27 22:32:31 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -6,8 +6,15 @@ end
 
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
-end
+  return
 
+<<<<<<< HEAD
   def engage
   puts state_log(star_date)
   end
+=======
+end
+  
+
+   
+>>>>>>> 3542d7c1e4795a90be36c71c74cadc009abe99bf
